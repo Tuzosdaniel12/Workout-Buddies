ca06e8b2b314f90abad089cd19666cb10e32986d
daniel <danielsoledad@gmail.com>
2021-01-17 22:32:55 -0800

    Adding database tables to project

---
+++ models/workouts.js
@@ -0,0 +1,36 @@
+module.exports = function(sequelize, DataTypes) {
+  const Workouts = sequelize.define("Workouts", {
+    // Work out title
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: "Body"
+    }
+  });
+
+  Workouts.associate = function(models) {
+    // Associating User with Workouts
+    Workouts.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+
+    // Workouts.belongsToMany(models.User, {
+    //   through: models.AllWorkouts
+    // });
+  };
+
+  return Workouts;
+};
