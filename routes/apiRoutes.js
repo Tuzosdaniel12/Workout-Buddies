0fc57cd0cfad075dbe5686f3793c4f57a48dda01
Tristan Bond <tristanebond@gmail.com>
2021-01-17 16:18:49 -0800

    updated api routes methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { ConnectionTimedOutError } = require("sequelize");
+const db = require("../models");
 const router = require("express").Router();
 
 // When log in display a table of most recent work outs
@@ -10,27 +11,37 @@ const router = require("express").Router();
 // ability to create work outs
 
 //read for homepage
-router.get("/",function(req,res){
-
+router.get("/api/read/:id",function(req,res){
+    db.workouts.findAll({where:{userID:req.params.id}}).then(function(results){
+        res.json(results);
+    });
 });
 //create workout
 router.post("/api/create",function(req,res){
-
+    db.workouts.create(req.body).then(function(results){
+        res.json(results);
 });
 //read for all workouts
 router.get("/workouts",function(req,res){
-
+    db.workouts.findAll().then(function(results){
+        res.json(results);
 });
 //read workouts based on category
-reouter.get("/api/workouts/:category",function(req,res){
-
+router.get("/api/workouts/:category",function(req,res){
+    db.workouts.findAll({where:{category:req.params.category}}).then(function(results){
+        res.json(results);
+    });
 });
 //delete workout off hompage
-router.delete("/api/delete",function(req,res){
-
+router.delete("/api/delete/:id",function(req,res){
+    db.workouts.destroy({where:{id:req.params.id}}).then(function(results){
+        res.json(results);
 });
 //update personal workout
-router.put("api/update",function(req,res){
-
+router.put("api/update/:id",function(req,res){
+    db.workouts.update({req.body},{where:{id:req.params.id}}).then(function(results){
+        res.json(results);
 });
-//
+
+
+module.exports = router;
