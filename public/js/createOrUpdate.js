fed5b1b4be2160f7fe86596649b40a31053da013
Tristan Bond <tristanebond@gmail.com>
2021-01-23 11:32:30 -0800

    added js for create and update

---
+++ public/js/createOrUpdate.js
@@ -0,0 +1,37 @@
+const title = $("#nameOfTheWorkoutInput1")
+  .val()
+  .trim();
+const category = $("#categoryInput1")
+  .val()
+  .trim();
+const description = $("#exerciseprogramTextarea1")
+  .val()
+  .trim();
+const CUBtn = $("#CUBtn");
+
+CUBtn.on("click", event => {
+  if (event.target.data("id")) {
+    updateWO(event.target.data("id"));
+  }
+    createWO();
+});
+
+function createWO() {
+  $.post("/api/workouts", {
+    title: title,
+    category: category,
+    description: description
+  }).then(() => {
+    window.location.replace("/members");
+  });
+}
+
+function updateWO(id) {
+  $.put("/api/workouts/" + id, {
+    title: title,
+    category: category,
+    description: description
+  }).then(() => {
+    window.location.replace("/members");
+  });
+}
