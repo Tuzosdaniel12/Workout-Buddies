40b51633ad2205775ce85779d5f6d5964be1a208
daniel <danielsoledad@gmail.com>
2021-01-24 22:21:39 -0800

    adding final memebers page to project

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -41,6 +41,9 @@ router.get("/activate", (req, res) => {
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get("/members", isAuthenticated, (req, res) => {
+  if (!req.user) {
+    res.redirect("/");
+  }
   res.render("members");
 });
 
