2ae65b97a74f68f5a86274c1d236bdd3f4e59dd3
Tristan Bond <tristanebond@gmail.com>
2021-01-26 20:58:44 -0800

    bmi routes

--- public/js/updateStats.js
+++ public/js/updateStats.js
@@ -1,5 +1,24 @@
 const updateBtn = $("#updatestatsformbtn");
+const BMI = require("../util/bmi");
 
-updateBtn.on("click", function(){
-    
-})
+updateBtn.on("click", event => {
+  event.preventDefault();
+  const weightInput = $("#weight-input");
+  const ageInput = $("#age-input");
+  const heightInput = $("#feet") * 12 + $("#inches");
+
+  const bmiCal = new BMI();
+
+  let bmi = bmiCal.getRequest(
+    ageInput,
+    weightInput * 0.453592,
+    heightInput * 2.54
+  );
+  bmi = Math.floor(bmi);
+
+  $.post("/api/bmi", {
+    bmi: bmi
+  }).then(() => {
+    window.location.replace("/progress");
+  });
+});
--- routes/bmiRoutes.js
+++ routes/bmiRoutes.js
@@ -2,13 +2,14 @@ const db = require("../models");
 const router = require("express").Router();
 
 router.get("/api/bmi", (req, res) => {
-  db.BMIs.findOne({ where: { userID: req.body.userID } }).then(results => {
+  db.BMI.findOne({ where: { userID: req.body.userID } }).then(results => {
     res.json(results);
   });
 });
 
 router.post("/api/bmi", (req, res) => {
-  db.BMIs.create({
+  db.BMI.create({
+    bmi: req.body.bmi,
     UserId: req.user.id
   }).then(results => {
     res.json(results);
